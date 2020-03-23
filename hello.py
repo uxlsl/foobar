26fe344bcc7b16f9d704c7728d12a336afa92e4e
lin <linsl2018@163.com>
2020-03-23 14:31:59 +0800

    modify hello.py

--- hello.py
+++ hello.py
@@ -4,6 +4,7 @@
 # Description   :
 # Author        :
 # Creation Date : 2020-03-23
-# Last Modified : Mon 23 Mar 2020 02:23:56 PM CST
+# Last Modified : Mon 23 Mar 2020 02:31:49 PM CST
 # Created By    : lsl
 print("hello world!")
+print("my name is lin")
