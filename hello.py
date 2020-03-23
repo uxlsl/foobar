1c3dd9bbc4501900506003778b271858a83701be
lin <linsl2018@163.com>
2020-03-23 14:26:00 +0800

    hello world

---
+++ hello.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*-coding:utf-8-*-
+# File Name     : hello.py
+# Description   :
+# Author        :
+# Creation Date : 2020-03-23
+# Last Modified : Mon 23 Mar 2020 02:23:56 PM CST
+# Created By    : lsl
+print("hello world!")
